f9f0e703dfc313ca18f90245bb39a7a8952bf226
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2021-08-06 11:07:04 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,22 @@
 module.exports = {
   apps : [{
     name   : "picciotto-xm",
-    script : "./server.js",
+    script : "server.js",
   }],
 
   deploy : {
     production: {
-      user: 'supervisor',
-      host: '151.106.109.41',
-      ref: 'start-server',
-      repo: 'git@github.com:xavierpicciotto/picciotto-server.git',
-      path: '/home/supervisor/test',
-      'pre-deploy-local':'',
-      'post-deploy':'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup':''
+      key: "C:/Users/xav/.ssh",
+      user: "supervisor",
+      host: ["151.106.109.41"],
+      ssh_options: "StrictHostKeyChecking=no",
+      ref: "origin/start-server",
+      repo: "git@github.com:xavierpicciotto/picciotto-server.git",
+      path: "/home/supervisor",
+      'pre-setup': "apt-get install git; ls -la",
+      'post-setup': "ls -la",
+      'pre-deploy-local': "echo'command local'",
+      'post-deploy':"npm install && pm2 reload ecosystem.config.js --env production",
     }
   }
 }
