abe6bda119f26b4c9d1a097040463a3cd43b03e6
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2021-08-09 21:05:26 +0200

    mise en place de rendu html server pour la home page
    update du server configuration d'un rendu html coter server et installation module pour envoyer des emails

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
-const app = express()
+const nodemailer = require('nodemailer')
+const exphbs = require('express-handlebars')
+const path = require('path')
+const db = require('./models/index')
 
+const app = express()
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+
+//View engine setup
+app.engine('handlebars', exphbs())
+app.set('view engine','handlebars')
+
+db.sequelize.sync()
+
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin','*'/*https://xavier-picciotto.com/*/)
@@ -11,12 +23,8 @@ app.use((req, res, next) => {
     next()
 })
 
-const db = require('./models/index')
-
-db.sequelize.sync()
-
-app.get('/',(req,res)=> {
-  res.status(200).send({message:'server piccciotto-xm en service'})
+app.get('/',(req, res)=> {
+  res.render('homeServer')/*status(200).send({message:'server piccciotto-xm en service'})*/
 })
 
 //CRUD USER
