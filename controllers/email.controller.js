7127ace9c226150ed01e8fbd578e5cd09963d2bd
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2021-08-17 19:34:33 +0200

    nouvelles fonctionaliter envois de fiche de contact
    suite au problème de sendingblue de mon portefolio pour envoyer des contacts j'ai décidé de me l'envoyer moi même

--- controllers/email.controller.js
+++ controllers/email.controller.js
@@ -10,9 +10,9 @@ async function codeExpiration(id) {
         }
     }).then(deleted => {
         if (deleted === 1) {
-            return console.log(`Code expiré id ${id}`)
+            return console.log(`Code expire id ${id}`)
         }
-        if(deleted === 0){
+        if (deleted === 0) {
             return
         }
         if (deleted !== 1) {
@@ -23,20 +23,20 @@ async function codeExpiration(id) {
 
 //Creer un demande de verification pour l'email renseigné
 exports.register = (req, res) => {
-    function code (num,length) {
+    function code(num, length) {
         //Génére des codes pour la verification d'email
         let code = Math.floor(Math.random() * num) + ''
         code = code.padStart(length, '0')
         return code
     }
-    const verifyCode = code(1001,"4") //fixe la valeur le code
-    const linkCode = code(10000000000000000001,"20")
+    const verifyCode = code(1001, "4") //fixe la valeur le code
+    const linkCode = code(10000000000000000001, "20")
     //appel la fonction de nodeMailer
     nodeMailer.main(req.body.email, `<html>
     <body>
         <h1>PICCIOTTO-XM</h1>
         <h2>Vérification de l'email ${req.body.email}</h2>
-        <h2>Methode n°1</h3>
+        <h2>Methode n°1</h2>
         <div class="code">
             <h1>CODE : ${verifyCode} </h1>
         </div>
@@ -53,7 +53,7 @@ exports.register = (req, res) => {
             //Délai d'expiration du Code de verif
             setTimeout(() => {
                 codeExpiration(email.id)
-            }, 300000/*5min*/)
+            }, 300000 /*5min*/ )
             return res.status(200).send({
                 message: `Email sauvegardé ${new Date}, un code de verification va vous êtres envoyé (valide 5min). Penser a regarder dans les spams si vous n'avez pas reçu l'email de confirmation`
             })
@@ -62,51 +62,101 @@ exports.register = (req, res) => {
         }))
     }).catch((err) => {
         res.status(400).send({
-            message: 'vérifier les donées saisies', err
+            message: 'vérifier les donées saisies',
+            err
         })
     });
 }
 
-exports.emailVerifcation =(req,res)=>{
+exports.emailVerifcation = (req, res) => {
     const Codeformat = /^[\d]{4}$/g
-    if(!Codeformat.test(req.body.code)){
-        return res.status(400).send({message: "Le format du code de vérification est éronné"})
+    if (!Codeformat.test(req.body.code)) {
+        return res.status(400).send({
+            message: "Le format du code de vérification est éronné"
+        })
     }
     Email.findOne({
         where: {
             email: req.body.email
         }
     }).then(email => {
-        if(email === null){
-            return res.status(404).send({message: `l'email: ${req.body.email} n'existe pas dans la database.`})
+        if (email === null) {
+            return res.status(404).send({
+                message: `l'email: ${req.body.email} n'existe pas dans la database.`
+            })
         }
         const verifyCode = email.verifyCode === req.body.code
-        if(verifyCode){
+        if (verifyCode) {
             codeExpiration(email.id)
-            return res.status(200).send({message: `L'adresse mail: ${email.email} est bien Validée, Le serveur est à titre démonstratif les données vont être effacées de la mémoire`})
+            return res.status(200).send({
+                message: `L'adresse mail: ${email.email} est bien Validée, Le serveur est à titre démonstratif les données vont être effacées de la mémoire`
+            })
         }
-        if(verifyCode !== true){
-            return res.status(400).send({message: `Le code est incorrect`})
+        if (verifyCode !== true) {
+            return res.status(400).send({
+                message: `Le code est incorrect`
+            })
         }
     }).catch(err => {
-        return res.status(500).send({message: err})
+        return res.status(500).send({
+            message: err
+        })
     })
 }
 
 exports.linkVerification = (req, res) => {
     const linkCodeFormat = /^[\d]{20}$/g
     const testFormat = linkCodeFormat.test(req.params.linkCode)
-    if(!testFormat){
-        return res.status(400).send({message: `Le lien est incorrect`})  
+    if (!testFormat) {
+        return res.status(400).send({
+            message: `Le lien est incorrect`
+        })
     }
     Email.findOne({
-        where:{
+        where: {
             linkCode: req.params.linkCode
         }
     }).then(email => {
         codeExpiration(email.id)
-        return res.status(200).send({message: `L'adresse mail: ${email.email} est bien Validée, Le serveur est à titre démonstratif les données vont être effacées de la mémoire`})
+        return res.status(200).send({
+            message: `L'adresse mail: ${email.email} est bien Validée, Le serveur est à titre démonstratif les données vont être effacées de la mémoire`
+        })
     }).catch(err => {
-        return res.status(404).send({message: "Le lien n'est associé a aucun email en mémoire"})
+        return res.status(404).send({
+            message: "Le lien n'est associé a aucun email en mémoire"
+        })
+    })
+}
+
+exports.relayContact = (req, res) => {
+    function testFormat(data) {
+        if(typeof(data) !== "string"){
+            return 'invalide'
+        } else {
+            return 'valide'  
+        }
+    }
+    if(testFormat(req.body.name) === 'invalide' || testFormat(req.body.company) === 'invalide' || testFormat(req.body.message) === 'invalide'){
+        return res.status(400).send({
+            message: 'Veuillez seulement utiliser des lettres des chiffres et (.:-\'_)'
+        })
+    }
+    nodeMailer.main('xavierpicciotto@gmail.com', `<html>
+    <body>
+        <h1>Contact Portfolio</h1>
+        <h2>email : ${req.body.email}</h2>
+        <h3>nom: ${req.body.name}</h3>
+        <h3>entreprise: ${req.body.company}</h3>
+        <h2>Message</h2>
+        <h3>${req.body.message}</h3>
+    </body>
+    </html>`).then(() => {
+        return res.status(200).send({
+            message: `Votre fiche de contact a bien été transmise`
+        })
+    }).catch(err => {
+        return res.status(500).send({
+            message: "Un problème est survenu lors de l'envoi de la fiche : " + err
+        })
     })
 }
--- routes/email.routes.js
+++ routes/email.routes.js
@@ -11,5 +11,6 @@ module.exports = function (app) {
   });
     app.post("/api/email/register",emailFormat,controller.register)
     app.post("/api/email/validate",emailFormat,controller.emailVerifcation)
-    app.get("/api/email/validate/:linkCode",controller.linkVerification) 
+    app.get("/api/email/validate/:linkCode",controller.linkVerification)
+    app.post("/api/email/contact",emailFormat,controller.relayContact) 
 }
