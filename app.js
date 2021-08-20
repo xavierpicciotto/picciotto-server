cab46360cb6b405a8e1be8919dd42b5e56098d9c
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2021-08-20 15:44:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*' /*https://xavier-picciotto.com/*/ )
-  res.setHeader('Access-Controle-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Controle-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, x-access-token')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
   next()
 })
